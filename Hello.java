72da6c5d505282b6c0c89c39855a3fe29f174ff7
Keerthana-dhara <68287451+Keerthana-dhara@users.noreply.github.com>
2024-10-09 14:41:25 +0530

    Update Hello.java
    changes committed

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@
 import java.util.*;
 public class Hello {
 public static void main(String args[]){
-    System.out.print("hello everyone");
+    System.out.println("hello everyone");
+    System.out.println("Welcome to github");
 }
 }
